f08045665720bafe6b16a2ab30dace4fad964dc4
KhogaEslam <KhogaEslam@gmail.com>
2017-05-06 17:59:40 +0200

    Khoga's Work

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
 const MongoClient = require("mongodb").MongoClient;
+const assert = require('assert');
 
-const url = "mongodb://localhost:27017/test";
+const url = "mongodb://localhost:27017/chatdb";
 
 var db = null;
+
+//Array contains objects [{'username' : username, 'msgContent' : msgContent, 'msgTime' : msgTime}]
+var publicMessages = [];
+var retPublicChat = [];
 
 app.use('/node_modules',express.static(__dirname + '/node_modules'));
 app.use(bodyParser.json())
@@ -18,13 +23,24 @@ MongoClient.connect(url, function(err, dbc) {
     }
     else {
         console.log("Connected successfully");
-        server.listen(8080);
         db = dbc;
+        server.listen(8080);
+
+        /*Just for testing*/
+        // savePublicChat({'username' : 'username1', 'msgContent' : 'msgContent1', 'msgTime' : 'msgTime1'});
+        // savePublicChat({'username' : 'username2', 'msgContent' : 'msgContent2', 'msgTime' : 'msgTime2'});
+        // savePublicChat({'username' : 'username3', 'msgContent' : 'msgContent3', 'msgTime' : 'msgTime3'});
+        //
+        //
+        // publicMessages.push({'username' : 'username4', 'msgContent' : 'msgContent4', 'msgTime' : 'msgTime4'});
+        // publicMessages.push({'username' : 'username5', 'msgContent' : 'msgContent5', 'msgTime' : 'msgTime5'});
+        // publicMessages.push({'username' : 'username6', 'msgContent' : 'msgContent6', 'msgTime' : 'msgTime6'});
+        // saveBulkPublicChat();
     }
 })
 
 /**
- * 
+ *
  */
 
 function buildAndCondition(username, secondArgument, email) {
@@ -48,3 +64,56 @@ function userAlreadyExist(username, password, email = false) {
     db.collection('users').find({})
 }
 
+/******************************************************************************/
+/*
+* Save One Public Message
+*/
+function savePublicChat(msg){
+  console.log('save one');
+  db.collection('publicChat').insertOne(msg)
+  .then(function(result) {
+    // process result
+    //console.log(result);
+  });
+}
+
+/******************************************************************************/
+/*
+* save Bulck array of Messages
+*/
+function saveBulkPublicChat(){
+  console.log('save bulk');
+  db.collection('publicChat').insertMany(publicMessages)
+  .then(function(result) {
+    // process result
+    //console.log(result);
+  });
+}
+
+/******************************************************************************/
+/*
+* get all chat messages
+*/
+var getBulkPublicChat = function(db, callback) {
+  console.log('get bulck');
+   var cursor = db.collection('publicChat').find();
+   cursor.each(function(err, chat) {
+      assert.equal(err, null);
+      if (chat != null) {
+         retPublicChat.push(chat);
+      } else {
+         callback();
+      }
+   });
+};
+/*
+to use:
+*/
+
+app.post('/api/getall',function(request, response){
+    console.log("NODE getall");
+      getBulkPublicChat(db, function() {
+          //db.close();
+          response.send(retPublicChat);
+      });
+});
