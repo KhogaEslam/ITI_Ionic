3b16c1be44b7299cb81bc9c15bb9088a9b5bcf98
mohamed_elalem <mohamed.el.alem.2017@gmail.com>
2017-05-06 14:27:40 +0200

    Adding extra modules

--- server/server.js
+++ server/server.js
@@ -3,3 +3,19 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017/test";
+
+app.use('/node_modules',express.static(__dirname + '/node_modules'));
+app.use(bodyParser.json())
+
+MongoClient.connect(url, function(err, db) {
+    if(err) {
+        console.log("Couldn't connect to mongodb", err);
+    }
+    else {
+        console.log("Connected successfully");
+        server.listen(8080);
+    }
+})
