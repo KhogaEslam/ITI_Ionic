b4d0de65efb67a558dd413e786e70fe09f1f7b4e
MernaShakerAttia <merna_shaker@yahoo.com>
2017-05-06 20:33:30 +0200

    Merna's Work

--- ayesm/www/js/controller/home.js
+++ ayesm/www/js/controller/home.js
@@ -1,8 +1,6 @@
 angular.module('chatApp').controller('home', function($state) {
   if (localStorage.getItem('remember')=="true") {
     console.log(localStorage.getItem('remember'));
-    console.log('toz');
     $state.go('app.activeUser');
-    console.log('eslam');
   }
 })
