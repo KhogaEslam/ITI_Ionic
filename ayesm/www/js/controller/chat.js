46e2411a861dbb9764a5e94fb488618b5602c48c
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-05-07 20:23:47 +0200

    Ay bdngan

--- ayesm/www/js/controller/chat.js
+++ ayesm/www/js/controller/chat.js
@@ -1,26 +1,36 @@
-angular.module('chatApp').controller("publicChat", function($scope, $http, Chat, $state, $rootScope, $ionicLoading) {
+angular.module('chatApp').controller("publicChat", function($scope,  $ionicScrollDelegate, $timeout, $http, Chat, $state, $rootScope, $ionicLoading) {
   $scope.chat = {};
+  $scope.activeusersmessages = {};
+  $scope.currentuser = {};
   //Array contains objects [{'username' : username, 'msgContent' : msgContent, 'msgTime' : msgTime}]
   var username = JSON.parse(localStorage.getItem("username"));
-  // Chat.StartPublicChat(username,"");
+
   $scope.sendMessage = function() {
     // console.log($scope.chat.message);
-    Chat.StartPublicChat(username, $scope.chat.message);
-    $scope.chat.message = "";
-
+    if($scope.chat.message != ''){
+      //Chat.StartPublicChat(username, $scope.chat.message);
+      socket.emit("public_message", username, $scope.chat.message);
+      $scope.chat.message = "";
+    }
   }
 
   Chat.getAllMessage().then(function(data) {
     console.log("data", data);
     $scope.activeusersmessages = data;
     $scope.currentuser = username;
-
+    $ionicScrollDelegate.scrollBottom();
   }, function(err) {
     console.log(err);
     console.log(JSON.stringify(err));
-
   });
 
+  socket.on('messages',function(msgs){
+    $timeout(function(){
+      $scope.activeusersmessages.push(msgs);
+      console.log("all active users msgs",msgs);
+      $ionicScrollDelegate.scrollBottom();
+    })
+  });
 
   // private_message
   // $scope.sendPrivateMessage = function() {
--- ayesm/www/js/factories/Chat.js
+++ ayesm/www/js/factories/Chat.js
@@ -1,59 +1,59 @@
-angular.module('chatApp').factory("Chat",function($q,$http){
+angular.module('chatApp').factory("Chat", function($q, $http) {
 
-return {
+  return {
 
-  getAllActiveUser:function(data){
-  			var def =$q.defer();
-  			$http({
-  				url:'http://172.16.5.7:8080/api/active' ,
-  				method:'POST',
-          data: data,
+    getAllActiveUser: function(data) {
+      var def = $q.defer();
+      $http({
+        url: 'http://172.16.5.7:8080/api/active',
+        method: 'POST',
+        data: data,
 
-  			}).then(function(res){
-  				console.log(res);
-  				if(res.data.code == 5){
-  					def.resolve(res.data)
-  				}else{
-  					def.reject('there is no data ')
-  				}
+      }).then(function(res) {
+        console.log(res);
+        if (res.data.code == 5) {
+          def.resolve(res.data)
+        } else {
+          def.reject('there is no data ')
+        }
 
-  			},function(err){
-  				// console.log(err);
-  				def.reject(err);
-  			})
-  			return def.promise ;
+      }, function(err) {
+        // console.log(err);
+        def.reject(err);
+      })
+      return def.promise;
 
-  		},
-      StartPublicChat:function(username,message){
-          socket.emit("public_message",username,message);
+    },
+    StartPublicChat: function(username, message) {
+      socket.emit("public_message", username, message);
+    },
 
-        },
+    getAllMessage: function() {
+      var def = $q.defer();
+      $http({
+        url: 'http://172.16.5.7:8080/api/getall',
+        method: 'POST',
+      }).then(function(res) {
+        console.log("all message", res);
+        if (res.data.length) {
+          def.resolve(res.data)
+        } else {
+          def.reject('there is no data ')
+        }
 
-        getAllMessage:function(){
-              var def =$q.defer();
-              $http({
-                url:'http://172.16.5.7:8080/api/getall' ,
-                method:'POST',
-              }).then(function(res){
-                console.log("all message",res);
-                if(res.data.length){
-                  def.resolve(res.data)
-                }else{
-                  def.reject('there is no data ')
-                }
+      }, function(err) {
+        // console.log(err);
+        def.reject(err);
+      })
+      return def.promise;
 
-              },function(err){
-                // console.log(err);
-                def.reject(err);
-              })
-              return def.promise ;
+    },
 
-            },
-
-            // StartPrivateChat:function(username,message){
-            //     socket.emit("private_message",username,message);
-            //
-            //   }
+    // StartPrivateChat:function(username,message){
+    //     socket.emit("private_message",username,message);
+    //
+    //   }
 
 
-}})
+  }
+})
